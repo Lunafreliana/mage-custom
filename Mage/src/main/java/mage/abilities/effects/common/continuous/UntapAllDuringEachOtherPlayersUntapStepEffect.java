eb1ba85f889978da26c8944790ac87bdf561a16c
Lunafreliana <paige.mathews@gmail.com>
2026-09-13 11:44:50 +0200

    Implement Horizon Boughs plane

--- Mage/src/main/java/mage/abilities/effects/common/continuous/UntapAllDuringEachOtherPlayersUntapStepEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/continuous/UntapAllDuringEachOtherPlayersUntapStepEffect.java
@@ -14,16 +14,23 @@ import mage.game.permanent.Permanent;
 public class UntapAllDuringEachOtherPlayersUntapStepEffect extends ContinuousEffectImpl {
 
     private final FilterPermanent filter;
+    private final boolean eachPlayersUntapStep;
 
     public UntapAllDuringEachOtherPlayersUntapStepEffect(FilterPermanent filter) {
+        this(filter, false);
+    }
+
+    public UntapAllDuringEachOtherPlayersUntapStepEffect(FilterPermanent filter, boolean eachPlayersUntapStep) {
         super(Duration.WhileOnBattlefield, Outcome.Untap);
         this.filter = filter;
+        this.eachPlayersUntapStep = eachPlayersUntapStep;
         staticText = setStaticText();
     }
 
     protected UntapAllDuringEachOtherPlayersUntapStepEffect(final UntapAllDuringEachOtherPlayersUntapStepEffect effect) {
         super(effect);
         this.filter = effect.filter;
+        this.eachPlayersUntapStep = effect.eachPlayersUntapStep;
     }
 
     @Override
@@ -33,7 +40,9 @@ public class UntapAllDuringEachOtherPlayersUntapStepEffect extends ContinuousEff
 
     @Override
     public boolean apply(Layer layer, SubLayer sublayer, Ability source, Game game) {
-        if (layer == Layer.RulesEffects && game.getTurnStepType() == PhaseStep.UNTAP && !source.isControlledBy(game.getActivePlayerId())) {
+        if (layer == Layer.RulesEffects
+                && game.getTurnStepType() == PhaseStep.UNTAP
+                && (eachPlayersUntapStep || !source.isControlledBy(game.getActivePlayerId()))) {
             Integer appliedTurn = (Integer) game.getState().getValue(source.getSourceId() + "appliedTurn");
             if (appliedTurn == null) {
                 appliedTurn = 0;
@@ -70,7 +79,9 @@ public class UntapAllDuringEachOtherPlayersUntapStepEffect extends ContinuousEff
             sb.append("all ");
         }
         sb.append(filter.getMessage());
-        sb.append(" during each other player's untap step");
+        sb.append(eachPlayersUntapStep
+                ? " during each player's untap step"
+                : " during each other player's untap step");
         return sb.toString();
     }
 }
--- Mage/src/main/java/mage/constants/Planes.java
+++ Mage/src/main/java/mage/constants/Planes.java
@@ -17,6 +17,7 @@ public enum Planes {
     PLANE_FIELDS_OF_SUMMER("FieldsOfSummerPlane", "Plane - Fields of Summer"),
     PLANE_GAVONY("GavonyPlane", "Plane - Gavony"),
     PLANE_HEDRON_FIELDS_OF_AGADEEM("HedronFieldsOfAgadeemPlane", "Plane - Hedron Fields of Agadeem"),
+    PLANE_HORIZON_BOUGHS("HorizonBoughsPlane", "Plane - Horizon Boughs"),
     PLANE_LAKE_SILENCIO("LakeSilencioPlane", "Plane - Lake Silencio", "WHO"),
     PLANE_LETHE_LAKE("LetheLakePlane", "Plane - Lethe Lake"),
     PLANE_NAYA("NayaPlane", "Plane - Naya"),
