2478fbd88a33fa58acef4cc21e0f34e7e71a7d7e
Lunafreliana <paige.mathews@gmail.com>
2026-09-13 16:39:58 +0200

    Implement Bowie Base One plane

--- Mage/src/main/java/mage/constants/Planes.java
+++ Mage/src/main/java/mage/constants/Planes.java
@@ -12,6 +12,7 @@ public enum Planes {
     PLANE_BAD_WOLF_BAY("BadWolfBayPlane", "Plane - Bad Wolf Bay", "WHO"),
     PLANE_BANT("BantPlane", "Plane - Bant"),
     PLANE_BESIEGED_VIKING_VILLAGE("BesiegedVikingVillagePlane", "Plane - Besieged Viking Village", "WHO"),
+    PLANE_BOWIE_BASE_ONE("BowieBaseOnePlane", "Plane - Bowie Base One", "WHO"),
     PLANE_ELOREN_WILDS("ElorenWildsPlane", "Plane - Eloren Wilds"),
     PLANE_EDGE_OF_MALACOL("EdgeOfMalacolPlane", "Plane - Edge of Malacol"),
     PLANE_FEEDING_GROUNDS("FeedingGroundsPlane", "Plane - Feeding Grounds"),
