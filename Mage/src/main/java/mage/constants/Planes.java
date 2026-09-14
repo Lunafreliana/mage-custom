d96af64649b9ec220d615c2d854bf51024134659
Lunafreliana <paige.mathews@gmail.com>
2026-09-14 05:40:44 +0200

    Implement Mondassian Colony Ship plane

--- Mage/src/main/java/mage/constants/Planes.java
+++ Mage/src/main/java/mage/constants/Planes.java
@@ -34,6 +34,7 @@ public enum Planes {
     PLANE_LAKE_SILENCIO("LakeSilencioPlane", "Plane - Lake Silencio", "WHO"),
     PLANE_LETHE_LAKE("LetheLakePlane", "Plane - Lethe Lake"),
     PLANE_LLANOWAR("LlanowarPlane", "Plane - Llanowar"),
+    PLANE_MONDASSIAN_COLONY_SHIP("MondassianColonyShipPlane", "Plane - Mondassian Colony Ship", "WHO"),
     PLANE_NAYA("NayaPlane", "Plane - Naya"),
     PLANE_NEPHALIA("NephaliaPlane", "Plane - Nephalia"),
     PLANE_NYX("NyxPlane", "Plane - Nyx", "MOC"),
