39abb39d11d440a8d9075be2b025c405eafc1c1a
Lunafreliana <paige.mathews@gmail.com>
2026-09-15 18:01:26 +0200

    Implement Littjara plane

--- Mage/src/main/java/mage/constants/Planes.java
+++ Mage/src/main/java/mage/constants/Planes.java
@@ -52,6 +52,7 @@ public enum Planes {
     PLANE_ISLE_OF_VESUVA("IsleOfVesuvaPlane", "Plane - Isle of Vesuva", "MOC"),
     PLANE_LAKE_SILENCIO("LakeSilencioPlane", "Plane - Lake Silencio", "WHO"),
     PLANE_LETHE_LAKE("LetheLakePlane", "Plane - Lethe Lake"),
+    PLANE_LITTJARA("LittjaraPlane", "Plane - Littjara", "MOC"),
     PLANE_LLANOWAR("LlanowarPlane", "Plane - Llanowar"),
     PLANE_MONDASSIAN_COLONY_SHIP("MondassianColonyShipPlane", "Plane - Mondassian Colony Ship", "WHO"),
     PLANE_MULTIVERSAL_HIGH_COUNCIL("MultiversalHighCouncilPlane", "Plane - Multiversal High Council", "PUNK"),
