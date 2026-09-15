ca6eb57c13507409d2b264e59763d59d7c52e08f
Lunafreliana <paige.mathews@gmail.com>
2026-09-15 17:54:02 +0200

    Implement Happy Yargle Day cards

---
+++ Mage.Sets/src/mage/cards/y/YargleGoliathOfOtaria.java
@@ -0,0 +1,34 @@
+package mage.cards.y;
+
+import java.util.UUID;
+import mage.MageInt;
+import mage.cards.CardImpl;
+import mage.cards.CardSetInfo;
+import mage.constants.CardType;
+import mage.constants.SubType;
+import mage.constants.SuperType;
+
+/**
+ * @author TheElk801
+ */
+public final class YargleGoliathOfOtaria extends CardImpl {
+
+    public YargleGoliathOfOtaria(UUID ownerId, CardSetInfo setInfo) {
+        super(ownerId, setInfo, new CardType[]{CardType.CREATURE}, "{4}{U}");
+
+        this.supertype.add(SuperType.LEGENDARY);
+        this.subtype.add(SubType.FROG);
+        this.subtype.add(SubType.SPIRIT);
+        this.power = new MageInt(3);
+        this.toughness = new MageInt(9);
+    }
+
+    private YargleGoliathOfOtaria(final YargleGoliathOfOtaria card) {
+        super(card);
+    }
+
+    @Override
+    public YargleGoliathOfOtaria copy() {
+        return new YargleGoliathOfOtaria(this);
+    }
+}
