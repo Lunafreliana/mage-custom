a3a48f26a0cc068ef9722b7db9cb64c7bac5036c
Lunafreliana <paige.mathews@gmail.com>
2026-09-16 19:46:59 +0200

    Register downloadable Cyberman reminder artwork

--- Mage/src/main/java/mage/cards/repository/TokenRepository.java
+++ Mage/src/main/java/mage/cards/repository/TokenRepository.java
@@ -30,6 +30,7 @@ public enum TokenRepository {
     // - image name from tok/xmage folder
     // - additional card name for controller like "Morph: face up name"
     public static final String XMAGE_IMAGE_NAME_FACE_DOWN_MANUAL = "Face Down";
+    public static final String XMAGE_IMAGE_NAME_FACE_DOWN_CYBERMAN = "Cyberman";
     public static final String XMAGE_IMAGE_NAME_FACE_DOWN_MANIFEST = "Manifest";
     public static final String XMAGE_IMAGE_NAME_FACE_DOWN_CLOAK = "Cloak";
     public static final String XMAGE_IMAGE_NAME_FACE_DOWN_MORPH = "Morph";
@@ -186,7 +187,7 @@ public enum TokenRepository {
                             continue;
                         }
                         if (!tokenClassName.endsWith("Plane")) {
-                            errorsList.add("Tokens database: plane's class name must ends with [...Plane] word: " + line);
+                            errorsList.add("Tokens database: plane's class name must ends with [...Phenomenon] word: " + line);
                             continue;
                         }
                     }
@@ -271,6 +272,9 @@ public enum TokenRepository {
         // TODO: can't find backface's api url so use direct link from third party site instead (must be replaced to scryfall someday)
         res.add(createXmageToken(XMAGE_IMAGE_NAME_FACE_DOWN_MANUAL, 1, "https://upload.wikimedia.org/wikipedia/en/a/aa/Magic_the_gathering-card_back.jpg"));
 
+        // Cyberman reminder card (Doctor Who Tokens, TWHO #24)
+        res.add(createXmageToken(XMAGE_IMAGE_NAME_FACE_DOWN_CYBERMAN, 1, "https://api.scryfall.com/cards/twho/24/en?format=image"));
+
         // Copy
         // https://scryfall.com/search?q=include%3Aextras+unique%3Aprints+type%3Atoken+copy&unique=cards&as=grid&order=name
         // https://scryfall.com/search?q=oracleid%3A88c78601-87f0-45e7-b2e0-e7ffcfb1cb70+include%3Aextras&unique=art&as=grid&order=released
@@ -331,7 +335,7 @@ public enum TokenRepository {
 
         // Cloak
         // support only 1 image: https://scryfall.com/card/tmkm/21/a-mysterious-creature
-        // https://scryfall.com/search?q=oracleid%3A6481a124-6859-4f02-9fd3-b1302528dd2e+include%3Aextras&unique=art&as=grid&order=released
+        // https://scryfall.com/search?q=oracleid%3A6481a124-6859-4f02-9fd3-b1302528dd2e+include%3Aextras&unique%3Aart&as=grid&order=released
         res.add(createXmageToken(XMAGE_IMAGE_NAME_FACE_DOWN_CLOAK, 1, "https://api.scryfall.com/cards/tmkm/21/en?format=image"));
         res.add(createXmageToken(XMAGE_IMAGE_NAME_FACE_DOWN_CLOAK, 2, "https://api.scryfall.com/cards/tacr/8/en?format=image"));
 
