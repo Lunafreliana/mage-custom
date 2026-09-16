33e8e8043d7371486cf134e8d1ea0b16e3af7fdf
Lunafreliana <paige.mathews@gmail.com>
2026-09-16 19:45:55 +0200

    Use dedicated reminder artwork for face-down Cybermen

--- Mage/src/main/java/mage/abilities/effects/common/continuous/BecomesCybermanEffect.java
+++ Mage/src/main/java/mage/abilities/effects/common/continuous/BecomesCybermanEffect.java
@@ -2,12 +2,17 @@ package mage.abilities.effects.common.continuous;
 
 import mage.MageObjectReference;
 import mage.abilities.Ability;
+import mage.cards.repository.TokenInfo;
+import mage.cards.repository.TokenRepository;
 import mage.constants.CardType;
 import mage.constants.Duration;
 import mage.constants.Outcome;
 import mage.constants.SubType;
 import mage.game.Game;
 import mage.game.permanent.Permanent;
+import mage.game.permanent.token.EmptyToken;
+import mage.game.permanent.token.Token;
+import mage.util.CardUtil;
 
 /**
  * Applies both the canonical face-down characteristics and the additional
@@ -43,6 +48,20 @@ public class BecomesCybermanEffect extends BecomesFaceDownCreatureEffect {
         permanent.removeAllSubTypes(game);
         permanent.addCardType(game, CardType.ARTIFACT, CardType.CREATURE);
         permanent.addSubType(game, SubType.CYBERMAN);
+
+        // The base face-down effect supplies the shared card-back image. Replace
+        // only this Cyberman's image metadata with its dedicated reminder art.
+        TokenInfo reminder = TokenRepository.instance.findPreferredTokenInfoForXmage(
+                TokenRepository.XMAGE_IMAGE_NAME_FACE_DOWN_CYBERMAN, permanent.getId());
+        if (reminder != null) {
+            Token image = new EmptyToken();
+            image.setExpansionSetCode(reminder.getSetCode());
+            image.setCardNumber("0");
+            image.setImageFileName(reminder.getName());
+            image.setImageNumber(reminder.getImageNumber());
+            image.setUsesVariousArt(false);
+            CardUtil.copySetAndCardNumber(permanent, image);
+        }
         return true;
     }
 }
