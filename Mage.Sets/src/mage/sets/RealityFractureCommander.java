bc44675c3a0d34b0f90478805811b7a63ab8dd22
Lunafreliana <paige.mathews@gmail.com>
2026-09-17 15:33:27 +0200

    Implement Dack Fayden, Helping Hand

--- Mage.Sets/src/mage/sets/RealityFractureCommander.java
+++ Mage.Sets/src/mage/sets/RealityFractureCommander.java
@@ -20,6 +20,7 @@ public final class RealityFractureCommander extends ExpansionSet {
         this.hasBasicLands = false;
 
         cards.add(new SetCardInfo("Avacyn, Angel of Horror", 7, Rarity.RARE, mage.cards.a.AvacynAngelOfHorror.class));
+        cards.add(new SetCardInfo("Dack Fayden, Helping Hand", 4, Rarity.RARE, mage.cards.d.DackFaydenHelpingHand.class));
         cards.add(new SetCardInfo("Darksteel Angel", 98, Rarity.RARE, mage.cards.d.DarksteelAngel.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Darksteel Angel", 13, Rarity.RARE, mage.cards.d.DarksteelAngel.class, NON_FULL_USE_VARIOUS));
         cards.add(new SetCardInfo("Ob Nixilis, the Ascended", 5, Rarity.RARE, mage.cards.o.ObNixilisTheAscended.class));
---
+++ Mage.Tests/src/test/java/org/mage/test/cards/single/frc/DackFaydenHelpingHandTest.java
@@ -0,0 +1,45 @@
+package org.mage.test.cards.single.frc;
+
+import mage.constants.PhaseStep;
+import mage.constants.Zone;
+import org.junit.Test;
+import org.mage.test.serverside.base.CardTestMultiPlayerBaseWithRangeAll;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author TheElk801
+ */
+public class DackFaydenHelpingHandTest extends CardTestMultiPlayerBaseWithRangeAll {
+
+    private static final String dack = "Dack Fayden, Helping Hand";
+
+    @Test
+    public void testRevealGoadAndDistribute() {
+        skipInitShuffling();
+        addCard(Zone.HAND, playerA, dack);
+        addCard(Zone.BATTLEFIELD, playerA, "Plains", 6);
+        addCard(Zone.LIBRARY, playerA, "Forest");
+        addCard(Zone.LIBRARY, playerA, "Grizzly Bears");
+        addCard(Zone.LIBRARY, playerA, "Island");
+        addCard(Zone.LIBRARY, playerA, "Centaur Courser");
+        addCard(Zone.LIBRARY, playerA, "Plains");
+        addCard(Zone.LIBRARY, playerA, "Hill Giant");
+
+        castSpell(1, PhaseStep.PRECOMBAT_MAIN, playerA, dack);
+        addTarget(playerA, playerD);
+        addTarget(playerA, playerC);
+        addTarget(playerA, playerB);
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.PRECOMBAT_MAIN);
+        execute();
+
+        assertPermanentCount(playerD, "Hill Giant", 1);
+        assertPermanentCount(playerC, "Centaur Courser", 1);
+        assertPermanentCount(playerB, "Grizzly Bears", 1);
+        assertLibraryCount(playerA, 3);
+        assertTrue(getPermanent("Hill Giant").getGoadingPlayers().contains(playerA.getId()));
+        assertTrue(getPermanent("Centaur Courser").getGoadingPlayers().contains(playerA.getId()));
+        assertTrue(getPermanent("Grizzly Bears").getGoadingPlayers().contains(playerA.getId()));
+    }
+}
